f900a7ab87461883e4fd9b3e5c75a1b8381f1d76
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-02-05 19:10:55 +0100

    path fun

--- static_darwin_arm64.go
+++ static_darwin_arm64.go
@@ -3,7 +3,7 @@ package gomf
 /*
 #cgo CPPFLAGS: -DDUCKDB_STATIC_BUILD
 #cgo LDFLAGS: -lduckdb
-#cgo darwin,arm64 LDFLAGS: -lc++ -L${SRCDIR}/macos-arm64/
+#cgo darwin,arm64 LDFLAGS: -lc++ -L${SRCDIR}/../go-module-fun/macos-arm64/
 #include <duckdb.h>
 */
 import "C"
